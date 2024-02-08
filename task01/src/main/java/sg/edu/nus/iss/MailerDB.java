ae344de04f4400c1f852308fc91ed06d5486fe4b
Theophania <pha.randomacc@gmail.com>
2024-02-08 12:16:22 +0800

    print csv

--- task01/src/main/java/sg/edu/nus/iss/MailerDB.java
+++ task01/src/main/java/sg/edu/nus/iss/MailerDB.java
@@ -3,10 +3,7 @@ package sg.edu.nus.iss;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class MailerDB {
